17f38e2e9d99e84030b552caacb9f9cd1a5ecba3
Murat AKMAMMEDOV <muratx10@gmail.com>
2020-06-16 22:58:13 +0300

    refactor: fix bugs

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -3,14 +3,14 @@ const generateSelectionList = (
   tagName,
   parentNode,
   className = '',
-  textNode = false,
+  textNode = false
 ) => {
   const fragment = document.createDocumentFragment();
-  for (let i = 1; i <= quantity; i++) {
+  for (let i = 1; i <= quantity; i += 1) {
     const optionElem = document.createElement(tagName);
     optionElem.className = className;
-    tagName === 'option' ? optionElem.value = `${i}` : null;
-    textNode ? optionElem.textContent = `${textNode} ${i}` : null;
+    tagName === 'option' ? (optionElem.value = `${i}`) : null;
+    textNode ? (optionElem.textContent = `${textNode} ${i}`) : null;
     fragment.appendChild(optionElem);
   }
   parentNode.appendChild(fragment);
